eaf9034d8d1acc2cc685ee60b474ab5373851988
Jean20Cruz <jean.bujaida@gmail.com>
2025-08-30 09:49:17 -0600

    Update service-worker.js
    * ADICIÓN DE RUTA A LISTA DE PRODUCTOS

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,15 @@
+// service-worker.js corregido
 const CACHE_NAME = 'agroservicio-v1';
 const urlsToCache = [
-  './', // Esto es crucial para la página principal (index.html)
+  './', 
   './index.html',
   './manifest.json',
   './service-worker.js',
+  './productos.json', //
+  './script.js' //
 ];
 
 self.addEventListener('install', (event) => {
-  // Realiza la instalación
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
@@ -21,14 +23,10 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Devuelve la respuesta desde la caché si está disponible
         if (response) {
           return response;
         }
-        
-        // Si no está en la caché, realiza la solicitud a la red
         return fetch(event.request);
       })
   );
 });
-
